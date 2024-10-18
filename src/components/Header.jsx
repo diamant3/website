a0830c023f2f52ec8f0b23de348a9c7833f8cb4e
Diamond Rivero <diamant3@proton.me>
2024-10-18 18:08:10 +0800

    improve intro

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import Nav from './Nav.jsx';
 export function Header() {
     return (
         <header className="text-black font-mono">
-            <div className="flex flex-col justify-around items-center py-6">
+            <div className="flex flex-col justify-around items-center">
                 <Logo />
                 <Nav />
             </div>
--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -4,8 +4,9 @@ export function Introduction() {
     return (
         <>
             <div className="text-center font-bold font-mono">
-                <h1 className="md:text-8xl text-6xl py-3">College Student</h1>
-                <h2 className="md:text-2xl text-1xl text-white"><span className="bg-blue-600 rounded-md p-1">Hobbyist Polyglot Coder</span></h2>
+                <h1 className="md:text-8xl text-6xl pt-4">Diamond E. Rivero</h1>
+                <h2 className="md:text-6xl text-4xl">College Student</h2>
+                <button type="button" className="bg-blue-600 text-white rounded-md p-1 my-2">Hobbyist Polyglot Coder</button>
             </div>
         </>
     );
