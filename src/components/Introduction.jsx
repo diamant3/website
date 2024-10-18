80a8914887910c3f4684470854ffeac60ca9b07b
Diamond Rivero <diamant3@proton.me>
2024-10-18 17:15:55 +0800

    remove slate bg color introduction

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -3,7 +3,7 @@ import { Footer } from './Footer.jsx'
 export function Introduction() {
     return (
         <>
-            <div className="text-center font-bold font-mono bg-slate-100">
+            <div className="text-center font-bold font-mono">
                 <h1 className="md:text-8xl text-6xl py-3">College Student</h1>
                 <h2 className="md:text-2xl text-1xl text-white"><span className="bg-blue-600 rounded-md p-1">Hobbyist Polyglot Coder</span></h2>
             </div>
