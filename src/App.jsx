56f7ebbb0d9efb0238139fe2e9b0d25039b4c08d
Diamond Rivero <diamant3@proton.me>
2024-10-18 21:48:59 +0800

    Add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { Header } from './components/Header.jsx'
 import { Footer } from './components/Footer.jsx'
 import { Introduction } from './components/Introduction.jsx';
 
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
   return (
     <>
+      <Analytics />
       <Header />
       <Introduction />
       <Footer />
