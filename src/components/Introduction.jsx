6bf0b452b811cd5d6e3682bfc80c3f2c0c1645d8
Diamond Rivero <diamant3@proton.me>
2024-10-18 21:45:21 +0800

    Update Introduction.jsx

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -6,7 +6,7 @@ export function Introduction() {
             <div className="text-center font-bold font-mono">
                 <h1 className="md:text-8xl text-6xl pt-4">Diamond Rivero</h1>
                 <h2 className="md:text-6xl text-4xl">College Student</h2>
-                <button type="button" className="bg-blue-600 text-white rounded-md p-1 my-2">Hobbyist Polyglot Coder</button>
+                <button type="button" className="bg-blue-600 text-white rounded-md p-1 my-2">Hobbyist Polyglot Computer Programmer</button>
             </div>
         </>
     );
