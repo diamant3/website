19dac4be5b4a45ecca21d549ed079cbd1807ea37
Diamond Rivero <diamant3@proton.me>
2024-10-18 21:25:42 +0800

    improve footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,24 @@
 export function Footer() {
     return (
-        <div className="flex flex-col md:flex-row justify-evenly items-center py-10 font-mono">
+        <div className="flex flex-col md:flex-row justify-evenly items-center font-mono pt-4">
             <a
                 href="https://linkedin.com/in/diamant3"
-                className="text-2xl hover:text-blue-600"
+                className="text-2xl hover:text-blue-600 pt-3"
                 target="blank"
             >LinkedIn</a>
             <a
                 href="https://github.com/diamant3"
-                className="text-2xl hover:text-blue-600"
+                className="text-2xl hover:text-blue-600 pt-3"
                 target="blank"
             >GitHub</a>
             <a
                 href="https://x.com/me_diamant3"
-                className="text-2xl hover:text-blue-600"
+                className="text-2xl hover:text-blue-600 pt-3"
                 target="blank"
             >X(Twitter)</a>
             <a
                 href="mailto:diamant3@proton.me"
-                className="text-2xl hover:text-blue-600"
+                className="text-2xl hover:text-blue-600 pt-3"
                 target="blank"
             >E-Mail</a>
         </div>
