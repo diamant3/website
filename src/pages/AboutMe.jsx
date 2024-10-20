79952230d308ec214067dee897c60d712afb23a8
Diamond Rivero <diamant3@proton.me>
2024-10-20 23:16:41 +0800

    add pspdev as org

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -8,12 +8,14 @@ export function AboutMe() {
             name: "University of Makati",
             desc: "Bachelor of Science in Computer Science",
             year: "2019 - current",
+            website: "https://umak.edu.ph"
         },
         {
             id: 1,
             name: "MCA Montessori School",
             desc: "Information Communication and Technology",
             year: "2017 - 2019",
+            website: "https://mcams.edu.ph/"
         },
     ]
 
@@ -22,7 +24,18 @@ export function AboutMe() {
             id: 0,
             name: "Station Square East Commercial Corporation",
             desc: "On-the-Job Training in the Finance Department",
-            year: "2018(80 Hours)"
+            year: "2018(80 Hours)",
+            website: "#" // no website
+        }
+    ]
+
+    const organization = [
+        {
+            id: 0,
+            name: "PSPDEV",
+            desc: "Member / Contributor",
+            year: "2024 - Current",
+            website: "https://pspdev.github.io"
         }
     ]
 
@@ -32,6 +45,7 @@ export function AboutMe() {
                 name={edu.name}
                 description={edu.desc}
                 year={edu.year}
+                website={edu.website}
             />
         </div>
     );
@@ -42,6 +56,18 @@ export function AboutMe() {
                 name={job.name}
                 description={job.desc}
                 year={job.year}
+                website={job.website}
+            />
+        </div>
+    );
+
+    const organizationList = organization.map(org =>
+        <div key={org.id} className="w-full">
+            <Minicard
+                name={org.name}
+                description={org.desc}
+                year={org.year}
+                website={org.website}
             />
         </div>
     );
@@ -54,6 +80,9 @@ export function AboutMe() {
 
             <h1 className="text-4xl font-mono text-center text-blue-600 pt-2">Careers</h1>
             {careerList}
+
+            <h1 className="text-4xl font-mono text-center text-blue-600 pt-2">Organizations</h1>
+            {organizationList}
         </>
     );
 }
