199a3913c51a57f0dfad624c37042b1f0516731f
Diamond Rivero <diamant3@proton.me>
2025-01-06 20:32:59 +0800

    make rounded profile

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ export function Logo() {
         <figure className="pt-6 pb-4">
             <img 
                 src="https://avatars.githubusercontent.com/u/71203851?v=4"
-                className="w-24 h-24 sm:w-48 sm:h-auto"
+                className="w-24 h-24 sm:w-48 sm:h-auto rounded-full mx-auto"
             />
         </figure>
     );
