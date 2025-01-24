23eea70344e398885f2a237fb6b4972f57605198
Diamond Rivero <diamant3@proton.me>
2025-01-24 15:58:42 +0800

    ...

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -3,7 +3,7 @@ export function Introduction() {
         <>
             <div className="text-center font-bold font-mono">
                 <h1 className="md:text-8xl text-6xl pt-4">Diamond Rivero</h1>
-                <h2 className="md:text-6xl text-4xl">College Student</h2>
+                { /* <h2 className="md:text-6xl text-4xl">College Student</h2> */ }
                 <button type="button" className="bg-blue-600 text-white p-1 my-2">Hobbyist Polyglot Computer Programmer</button>
             </div>
         </>
